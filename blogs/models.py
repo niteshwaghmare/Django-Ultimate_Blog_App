07582ee67d534b98dc0b2193d3fb90457a02b767
Nitesh Waghmare <niteshw2@gmail.com>
2020-07-31 01:15:45 +0530

    Markdown feature added

--- blogs/models.py
+++ blogs/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils.html import mark_safe
+from markdown import markdown
+import readtime
 # Create your models here.
 
 STATUS = ((0, "Draft"), (1, "Publish"))
@@ -19,5 +21,12 @@ class BlogPost(models.Model):
     class Meta:
         ordering = ['-created_on']
 
+    
+    def get_content_as_markdown(self):
+        return mark_safe(markdown(self.content, safe_mode='escape'))
+
+    def get_read_time(self):
+        return readtime.of_text(self.content)
+    
     def __str__(self):
         return self.title
