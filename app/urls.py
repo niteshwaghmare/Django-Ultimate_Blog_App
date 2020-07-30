4816cd3ad590149caa7144f481f4ac254ed96a95
Nitesh Waghmare <niteshw2@gmail.com>
2020-07-31 00:33:44 +0530

    Styling done for blogs

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,8 @@
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.urls import path, re_path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
--- blogs/models.py
+++ blogs/models.py
@@ -9,6 +9,7 @@ STATUS = ((0, "Draft"), (1, "Publish"))
 class BlogPost(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    image = models.FileField(upload_to='blog_images/', null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
